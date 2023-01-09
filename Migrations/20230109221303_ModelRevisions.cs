82ced52965f3459af02dda4f7efd2860cfdde9a9
Leonardo Kiszewski <kiszewski1999@gmail.com>
2023-01-09 19:13:54 -0300

    Changes on Models

---
+++ Migrations/20230109221303_ModelRevisions.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelRevisions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Toppings",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Calories",
+                table: "Toppings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ToppingId",
+                table: "Pizzas",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pizzas_ToppingId",
+                table: "Pizzas",
+                column: "ToppingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pizzas_Toppings_ToppingId",
+                table: "Pizzas",
+                column: "ToppingId",
+                principalTable: "Toppings",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pizzas_Toppings_ToppingId",
+                table: "Pizzas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pizzas_ToppingId",
+                table: "Pizzas");
+
+            migrationBuilder.DropColumn(
+                name: "Calories",
+                table: "Toppings");
+
+            migrationBuilder.DropColumn(
+                name: "ToppingId",
+                table: "Pizzas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Toppings",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+        }
+    }
+}
